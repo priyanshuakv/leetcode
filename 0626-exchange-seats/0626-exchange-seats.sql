a113c1d7ea58ac45f7204176abd13e8b32f8202a
Priyanshu Verma <priyanshuakv@gmail.com>
2026-04-03 18:26:36 +0530

    Time: 632 ms (5.11%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,9 +1,15 @@
 # Write your MySQL query statement below
-select 
-    case  
-        when id %2 = 0 then id - 1
-        when id = (select max(id) from seat)   then id
-    else id + 1
-    end as id,
-student from seat 
-order by id asc;
+-- select 
+--     case  
+--         when id %2 = 0 then id - 1
+--         when id = (select max(id) from seat)   then id
+--     else id + 1
+--     end as id,
+-- student from seat 
+-- order by id asc;
+select id, 
+case 
+    when id % 2= 1 then coalesce(lead(student) over(order by id ), student)
+    else lag(student) over (order by id)
+    end as student 
+from seat;
